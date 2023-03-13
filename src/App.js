5522ba1dd41e342fbcee587f1abbbe3ace3ed712
JerryWski <jarek_wasilewski@yahoo.com>
2023-03-13 19:51:11 +0100

    setting up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 const App = () => {
   return (
-    <></>
-  )
+    <>
+      <div className="container">
+        <Navbar />
+        <Main />
+      </div>
+    </>
+  );
 };
 
 export default App;
