9953a201e804e4c84670a5cf8083863600e1c2bd
JerryWski <jarek_wasilewski@yahoo.com>
2023-03-19 12:00:32 +0100

    more logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
 import Dice from "./components/Dice";
+import { nanoid } from 'nanoid';
 
 const App = () => {
   const allNewDice = () => {
     const newArray = [];
     for (let i = 0; i < 10; i++) {
-      newArray.push(Math.floor(Math.random() * 6));
+      newArray.push({ 
+        value: Math.floor(Math.random() * 6), 
+        isHeld: false,
+        id: nanoid() 
+      });
     }
     return newArray;
   };
 
   const [arrayNumbers, setArrayNumbers] = useState(allNewDice());
 
-  const newArrayNumbers = arrayNumbers.map((arrayNumber, id) => (
-    <Dice value={arrayNumber} key={id}/>
+  const rollDice = () => {
+    setArrayNumbers(allNewDice());
+  };
+  const holdDice = (id) => {
+    console.log(id);
+  }
+  const newArrayNumbers = arrayNumbers.map((arrayNumber) => (
+    <Dice value={arrayNumber.value} key={arrayNumber.id} isHeld={arrayNumber.isHeld} holdDice={() => holdDice(arrayNumber.id)} />
   ));
-  console.log(newArrayNumbers);
+
 
   return (
     <main>
       <div className="dice-container">{newArrayNumbers}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 };
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,8 +1,11 @@
 import styles from "./Dice.module.css"
 
 const Dice = (props) => {
+  const styling = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+  }
   return (
-    <div className={styles.dice_face}>
+    <div onClick={props.holdDice} className={styles.dice_face} style={styling}>
       <h2 className={styles.dice_num}>{props.value}</h2>
     </div>
   );
