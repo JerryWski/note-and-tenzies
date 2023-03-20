6c547410c009e2ff6f45e6ad0123ab296dee11bd
JerryWski <jarek_wasilewski@yahoo.com>
2023-03-20 20:52:40 +0100

    working tenzies game

--- src/App.js
+++ src/App.js
@@ -1,39 +1,79 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
 import Dice from "./components/Dice";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
+import Confetti from 'react-confetti'
 
 const App = () => {
+  const generateNewDice = () => {
+    return {
+      value: Math.floor(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  };
+
   const allNewDice = () => {
     const newArray = [];
     for (let i = 0; i < 10; i++) {
-      newArray.push({ 
-        value: Math.floor(Math.random() * 6), 
-        isHeld: false,
-        id: nanoid() 
-      });
+      newArray.push(generateNewDice());
     }
     return newArray;
   };
 
   const [arrayNumbers, setArrayNumbers] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = arrayNumbers.every((arrayNumber) => arrayNumber.isHeld);
+    const firstValue = arrayNumbers[0].value;
+    const allSameValue = arrayNumbers.every(
+      (arrayNumber) => arrayNumber.value === firstValue
+    );
+    if(allHeld && allSameValue) {
+      setTenzies(true)
+      console.log('you won');
+    }
+  }, [arrayNumbers]);
 
   const rollDice = () => {
-    setArrayNumbers(allNewDice());
+    setArrayNumbers((oldArrayNumbers) =>
+      oldArrayNumbers.map((arrayNumber) => {
+        return arrayNumber.isHeld ? arrayNumber : generateNewDice();
+      })
+    );
   };
-  const holdDice = (id) => {
-    console.log(id);
-  }
-  const newArrayNumbers = arrayNumbers.map((arrayNumber) => (
-    <Dice value={arrayNumber.value} key={arrayNumber.id} isHeld={arrayNumber.isHeld} holdDice={() => holdDice(arrayNumber.id)} />
-  ));
 
+  const holdDice = (id) => {
+    setArrayNumbers((prevArrayNumbers) =>
+      prevArrayNumbers.map((arrayNumber) => {
+        return arrayNumber.id === id
+          ? { ...arrayNumber, isHeld: !arrayNumber.isHeld }
+          : arrayNumber;
+      })
+    );
+  };
+
+  const newArrayNumbers = arrayNumbers.map((arrayNumber) => (
+    <Dice
+      value={arrayNumber.value}
+      key={arrayNumber.id}
+      isHeld={arrayNumber.isHeld}
+      holdDice={() => holdDice(arrayNumber.id)}
+    />
+  ));
 
   return (
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{newArrayNumbers}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll
+        {tenzies === true ? " New Game ": "Roll" }
       </button>
     </main>
   );
